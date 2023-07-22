dc2f9059d68e4aa18f1a79663e182df1e82285cc
CChreem <ana.chreem@stone.com.br>
2023-07-22 11:09:24 -0300

    implementation

---
+++ CalculateSumAndAverage/CalculateSumAndAverage/Operations/Users/Classes/ControlNumbers.cs
@@ -0,0 +1,44 @@
+﻿using CalculateSumAndAverage.Operations.Users.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateSumAndAverage.Operations.Users.Classes
+{
+    public class ControlNumbers : IControlNumbers
+    {
+        public int GetTotalQuantity()
+        {
+            int quantity;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out quantity) && quantity >= 3 && quantity <= 10)
+                {
+                    return quantity;
+                }
+                else
+                {
+                    Console.WriteLine("Quantidade inválida. Digite um número entre 3 e 10.");
+                }
+            }
+        }
+
+        public double GetNumbers()
+        {
+            double numero;
+            while (true)
+            {
+                if (double.TryParse(Console.ReadLine(), out numero))
+                {
+                    return numero;
+                }
+                else
+                {
+                    Console.WriteLine("Número inválido. Digite um valor numérico válido.");
+                }
+            }
+        }
+    }
+}
--- CalculateSumAndAverage/CalculateSumAndAverage/Program.cs
+++ CalculateSumAndAverage/CalculateSumAndAverage/Program.cs
@@ -1,15 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CalculateSumAndAverage.Operations.Users.Classes;
+using CalculateSumAndAverage.Operations.Users.Interfaces;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CalculateSumAndAverage
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        var controlNumbers = new ControlNumbers();
+        var control = true;
+
+        while (control)
         {
-        }
+            Console.WriteLine("Digite de quantos números você quer calcular a soma e a média (entre 3 e 10):");
+            var quantity = controlNumbers.GetTotalQuantity();
+
+            var numbers = new double[quantity];
+            for (int i = 0; i < quantity; i++)
+            {
+                Console.WriteLine($"Digite o número {i + 1}:");
+                numbers[i] = controlNumbers.GetNumbers();
+            }
+
+            Console.WriteLine($"A soma dos números é: {numbers.Sum()}");
+            Console.WriteLine($"A média dos números é: {numbers.Average()}");
+            Console.WriteLine("Aperte 0 para sair ou qualquer tecla para continuar\n");
+            var response = Console.ReadLine();
+
+            if (response == "0")
+            {
+                break;
+            }
+        }       
     }
 }
